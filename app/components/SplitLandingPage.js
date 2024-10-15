0cc5081071f956021194b4869c825d67112ed848
Armand Soto <armand.e.soto@gmail.com>
2024-10-15 15:01:30 -0400

    Added Component and Images
    Updated Home to a Split Landing Page. Temporarily disabled Mission Statement. Added images for the split landing page.

---
+++ app/components/SplitLandingPage.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Link from 'next/link';
+
+
+const imgOverlay = `bg-center before:content-[''] before:absolute before:inset-0 before:bg-black before:bg-opacity-50`;
+
+
+const SplitLandingPage = () => {
+    return (
+        <div className="flex h-screen">
+            <div className={`relative w-1/2 bg-cover ${imgOverlay} flex items-center justify-center transition-all duration-300 hover:w-3/4 w-1/2 hover:before:bg-opacity-0 bg-[url('./assets/images/for-hire-img.jpg')]`}>
+                <div className="text-center text-white z-10">
+                    <h1 className="text-3xl font-bold">Job Seekers</h1>
+                    <p className="mt-4">Explore thousands of job listings and find your dream job</p>
+                    <Link href="./FindJobs/page.js">
+                        <button className="mt-6 inline-block bg-black bg-opacity-50 px-4 py-2 rounded text-white">Find Jobs</button>
+                    </Link>
+                </div>
+            </div>
+            <div className={`relative w-1/2 bg-cover ${imgOverlay} flex items-center justify-center transition-all duration-300 hover:w-3/4 w-1/2 hover:before:bg-opacity-0 bg-[url('./assets/images/hiring-img.jpg')]`}>
+                <div className="text-center text-white z-10">
+                    <h1 className="text-3xl font-bold">Employers</h1>
+                    <p className="mt-4">Post a job and find the perfect candidate for your company</p>
+                    <Link href="./FindJobs/page.js">
+                        <button className="mt-6 inline-block bg-black bg-opacity-50 px-4 py-2 rounded text-white">Employers</button>
+                    </Link>
+                </div>
+
+            </div>
+        </div>    
+    )
+}
+
+export default SplitLandingPage;
--- app/page.js
+++ app/page.js
@@ -1,23 +1,27 @@
 import Image from "next/image";
 import workImg1 from './assets/images/work-img1.jpg'
+import SplitLandingPage from "./components/SplitLandingPage";
+
+// Updated to a split landing page. Deciding what to do with the Mission statement page at the moment
 
 export default function Home() {
   return (
-    <main className="mt-16 p-12 flex w-full bg-gray-200">
-        {/* hardcoded string because this content will not change. Use props if we must reuse components */}
-        <section className="w-1/2 ">
-            <h1 className="text-4xl">Who We Are</h1>
-            <h2 className="text-2xl">Mission Statement</h2>
-            <p>
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Neque felis dignissim erat penatibus hendrerit hendrerit tincidunt. Morbi ornare magnis; scelerisque tellus mauris convallis suscipit. Natoque convallis dolor ligula at maximus eget placerat. Metus vel dapibus amet quisque vivamus? Nunc class donec integer tempus quis habitant consectetur bibendum id. Litora habitasse varius ornare quam ac. Finibus congue dolor fames, rhoncus facilisis nascetur porta purus mi.
-            </p>
-            <p>
-            Amattis viverra gravida; condimentum mattis nascetur. Fusce magnis nunc taciti posuere morbi. Potenti tortor dapibus natoque aliquam condimentum lacus maximus. Eros venenatis ante molestie pharetra urna rutrum. Proin dis etiam risus, pharetra lectus sem. Purus per eu proin sapien lacus lacinia laoreet sem. Odio magna cubilia vulputate consectetur facilisi accumsan.
-            </p>
-        </section>
-        <section className="w-1/2">
-            <img src="./assets/images/work-img1.jpg" className="rounded" alt="Description of Image" />
-        </section>
-    </main>
+    // <main className="mt-16 p-12 flex w-full bg-gray-200">
+    //     {/* hardcoded string because this content will not change. Use props if we must reuse components */}
+    //     <section className="w-1/2 ">
+    //         <h1 className="text-4xl">Who We Are</h1>
+    //         <h2 className="text-2xl">Mission Statement</h2>
+    //         <p>
+    //         Lorem ipsum odor amet, consectetuer adipiscing elit. Neque felis dignissim erat penatibus hendrerit hendrerit tincidunt. Morbi ornare magnis; scelerisque tellus mauris convallis suscipit. Natoque convallis dolor ligula at maximus eget placerat. Metus vel dapibus amet quisque vivamus? Nunc class donec integer tempus quis habitant consectetur bibendum id. Litora habitasse varius ornare quam ac. Finibus congue dolor fames, rhoncus facilisis nascetur porta purus mi.
+    //         </p>
+    //         <p>
+    //         Amattis viverra gravida; condimentum mattis nascetur. Fusce magnis nunc taciti posuere morbi. Potenti tortor dapibus natoque aliquam condimentum lacus maximus. Eros venenatis ante molestie pharetra urna rutrum. Proin dis etiam risus, pharetra lectus sem. Purus per eu proin sapien lacus lacinia laoreet sem. Odio magna cubilia vulputate consectetur facilisi accumsan.
+    //         </p>
+    //     </section>
+    //     <section className="w-1/2">
+    //         <img src="./assets/images/work-img1.jpg" className="rounded" alt="Description of Image" />
+    //     </section>
+    // </main>
+    <SplitLandingPage />
   );
 }
