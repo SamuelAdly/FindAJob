19af4f1c0bba3a8c5d2fb9a931b205ba3c98ec7c
Aiden Schulman <aidenhschulman@gmail.com>
2024-10-21 15:53:39 -0400

    updated routes

--- app/components/SplitLandingPage.js
+++ app/components/SplitLandingPage.js
@@ -12,7 +12,7 @@ const SplitLandingPage = () => {
                 <div className="text-center text-white z-10">
                     <h1 className="text-3xl font-bold">Job Seekers</h1>
                     <p className="mt-4">Explore thousands of job listings and find your dream job</p>
-                    <Link href="./FindJobs">
+                    <Link href="./find-jobs">
                         <button className="mt-6 inline-block bg-black bg-opacity-50 px-4 py-2 rounded text-white">Find Jobs</button>
                     </Link>
                 </div>
@@ -21,7 +21,7 @@ const SplitLandingPage = () => {
                 <div className="text-center text-white z-10">
                     <h1 className="text-3xl font-bold">Employers</h1>
                     <p className="mt-4">Post a job and find the perfect candidate for your company</p>
-                    <Link href="./FindJobs/page.js">
+                    <Link href="./find-jobs">
                         <button className="mt-6 inline-block bg-black bg-opacity-50 px-4 py-2 rounded text-white">Employers</button>
                     </Link>
                 </div>
